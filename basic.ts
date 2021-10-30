db7ea9c610408cee48648705a6d853331b5a9066
Arun kumar <79135781+arunravi03@users.noreply.github.com>
2021-10-30 07:52:38 -0500

    Add files via upload

---
+++ basic.ts
@@ -0,0 +1,20 @@
+// var message = 'HELLO';
+// message.toLowerCase();
+// console.log(message);
+// Calling 'message'
+// message();
+
+// function fn(x) {
+//     return x
+//   }
+
+//   console.log(fn("sd"));
+
+console.log("Hello world!");
+
+"use strict";
+function greet(person, date) {
+    console.log("Hello " + person + ", today is " + date.toDateString() + "!");
+}
+greet("Maddison", new Date());
+ 
